f007c48931e38505462a87c9c4b61696bcffcff1
Jelani Woods <jelani@firstdraft.com>
2019-09-25 14:15:04 -0500

    Starting point

---
+++ README.md
@@ -0,0 +1,10 @@
+# Refactoring MSM Queries with Methods
+
+[This Chapter contains detailed notes](https://chapters.firstdraft.com/chapters/843) for this project.
+
+## Setup
+
+1. Start the web server by running `bin/server`.
+1. Navigate to your live application preview.
+1. As you work, remember to navigate to `/git` and **commit often as you work.**
+1. `rails grade` to ensure everything still works.
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+  
+  get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
+  
+  get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  
+  get("/actors", { :controller => "actors", :action => "index" })
+  
+  get("/actors/:path_id", { :controller => "actors", :action => "show" })
+end
